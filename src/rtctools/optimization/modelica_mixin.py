XianwuXue-NOAA/rtc-tools

commit 10da03cbf09f94a3c30c55c9fe720be6cde4b76d

ModelicaMixin: Optimize seed()
A cast to ca.MX() is costly, and should therefore be avoided where
possible.

src/rtctools/optimization/modelica_mixin.py
@@ -293,23 +293,29 @@ class ModelicaMixin(OptimizationProblem):
 
         # Load seeds
         for var in itertools.chain(self.__pymoca_model.states, self.__pymoca_model.alg_states):
-            start = ca.MX(var.start)
-            if not var.fixed and not start.is_zero():
-                # If start contains symbolics, try substituting parameter values
-                if not start.is_constant():
-                    [start] = substitute_in_external([start], self.__mx['parameters'], parameter_values)
+            if var.fixed:
+                # Values will be set from import timeseries
+                continue
 
-                # If start is constant, seed it. Else, warn.
+            start = var.start
+
+            # NOTE: If start is symbolic, a cast to float will result in NaN
+            if float(start) != 0.0:
                 sym_name = var.symbol.name()
-                if start.is_constant():
-                    times = self.times(sym_name)
-                    start = var.python_type(var.start)
-                    s = Timeseries(times, np.full_like(times, start))
-                    if logger.getEffectiveLevel() == logging.DEBUG:
-                        logger.debug("ModelicaMixin: Seeded variable {} = {}".format(sym_name, start))
-                    seed[sym_name] = s
-                else:
-                    logger.error('ModelicaMixin: Could not resolve seed value for {}'.format(sym_name))
+
+                # If start contains symbolics, try substituting parameter values
+                if isinstance(start, ca.MX) and not start.is_constant():
+                    [start] = substitute_in_external([start], self.__mx['parameters'], parameter_values)
+                    if not start.is_constant():
+                        logger.error('ModelicaMixin: Could not resolve seed value for {}'.format(sym_name))
+
+                times = self.times(sym_name)
+                start = var.python_type(var.start)
+                s = Timeseries(times, np.full_like(times, start))
+                if logger.getEffectiveLevel() == logging.DEBUG:
+                    logger.debug("ModelicaMixin: Seeded variable {} = {}".format(sym_name, start))
+                seed[sym_name] = s
+
         return seed
 
     def variable_is_discrete(self, variable):

commit a764b37a3fdf55575949b806aea1b323ea3ac0fa

ModelicaMixin: Fix reading of symbolic start attributes
Regression in 10da03cb. Only for SX symbols does a symbolic expression
turn into a NaN. For symbolic MX expressions a RuntimeError is raised.
We also have to do the Python type conversion on the _substituted_
value, not the original MX value (which will result in a RuntimeError).

src/rtctools/optimization/modelica_mixin.py
@@ -303,8 +303,7 @@ class ModelicaMixin(OptimizationProblem):
 
             start = var.start
 
-            # NOTE: If start is symbolic, a cast to float will result in NaN
-            if float(start) != 0.0:
+            if isinstance(start, float) and start != 0.0 or isinstance(start, ca.MX):
                 sym_name = var.symbol.name()
 
                 # If start contains symbolics, try substituting parameter values
@@ -314,7 +313,7 @@ class ModelicaMixin(OptimizationProblem):
                         logger.error('ModelicaMixin: Could not resolve seed value for {}'.format(sym_name))
 
                 times = self.times(sym_name)
-                start = var.python_type(var.start)
+                start = var.python_type(start)
                 s = Timeseries(times, np.full_like(times, start))
                 if logger.getEffectiveLevel() == logging.DEBUG:
                     logger.debug("ModelicaMixin: Seeded variable {} = {}".format(sym_name, start))
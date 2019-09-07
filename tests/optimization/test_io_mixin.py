XianwuXue-NOAA/rtc-tools

commit 623fc2cf5fb07d2b0c0a2172a7d3e32b85b854a4

Refactor of IO Mixins
Large refactoring of the IO mixins (PIMixin and CSVMixin for both
SimulationProblems and OptimizationProblems.

We add a general DataStoreAccessor class extended by OptimizationProblem
and SimulationProblem, which is used to store all data read by the IO
mixins in a DataStore object. Higher lever mixins like PIMixin/CSVMixin
do not store their data themselves anymore. Of course users are still
free to override get_timeseries/set_timeseries and store data themselves
in their own mixins.

For both simulation and optimization we also add an IOMixin class for
which contains methods that were previously duplicated in their
respective PIMixin and CSVMixin classes. There is still quite a bit of
duplicate code between the IOMixin for optimization and the IOMixin for
simulation (and similarly for the respective PIMixins and CSVMixins),
but that is something a future commit will address.

src/rtctools/optimization/io_mixin.py
@@ -1,0 +1,294 @@
+import bisect
+import logging
+import warnings
+from abc import ABCMeta, abstractmethod
+
+import casadi as ca
+
+import numpy as np
+
+from rtctools._internal.alias_tools import AliasDict
+from rtctools._internal.caching import cached
+from rtctools.optimization.optimization_problem import OptimizationProblem
+from rtctools.optimization.timeseries import Timeseries
+
+logger = logging.getLogger("rtctools")
+
+
+class IOMixin(OptimizationProblem, metaclass=ABCMeta):
+    """
+    Base class for all IO methods of optimization problems.
+    """
+
+    def __init__(self, **kwargs):
+        # Call parent class first for default behaviour.
+        super().__init__(**kwargs)
+
+        # Additional output variables
+        self.__output_timeseries = set()
+        self.__equidistant = False
+
+    def pre(self) -> None:
+        # Call parent class first for default behaviour.
+        super().pre()
+
+        # Call read method to read all input
+        self.read()
+
+        # Check equidistancy on read input data
+        if self.io.datetimes:
+            self.__equidistant = len(set(np.diff(self.io.datetimes))) == 1
+
+    @abstractmethod
+    def read(self) -> None:
+        """
+        Reads input data from files
+        """
+        pass
+
+    def post(self) -> None:
+        # Call parent class first for default behaviour.
+        super().post()
+
+        # Call write method to write all output
+        self.write()
+
+    @abstractmethod
+    def write(self) -> None:
+        """"
+        Writes output data to files
+        """
+        pass
+
+    def times(self, variable=None) -> np.ndarray:
+        """
+        Returns the times in seconds from the reference datetime onwards.
+
+        :param variable:
+        """
+        times_sec = self.io.times_sec
+        t_idx = bisect.bisect_left(times_sec, 0.0)
+        return times_sec[t_idx:]
+
+    @property
+    def equidistant(self):
+        return self.__equidistant
+
+    @property
+    def ensemble_size(self):
+        return self.io.ensemble_size
+
+    def get_timeseries(self, variable: str, ensemble_member: int = 0) -> Timeseries:
+        return Timeseries(*self.io.get_timeseries_sec(variable, ensemble_member))
+
+    def set_timeseries(
+            self,
+            variable: str,
+            timeseries: Timeseries,
+            ensemble_member: int = 0,
+            output: bool = True,
+            check_consistency: bool = True):
+
+        def stretch_values(values, t_pos):
+            # Construct a values range with preceding and possibly following nans
+            new_values = np.full(self.io.times_sec.shape, np.nan)
+            new_values[t_pos:] = values
+            return new_values
+
+        if output:
+            self.__output_timeseries.add(variable)
+
+        if isinstance(timeseries, Timeseries):
+            if len(timeseries.values) != len(timeseries.times):
+                raise ValueError('IOMixin: Trying to set timeseries {} with times and values that are of '
+                                 'different length (lengths of {} and {}, respectively).'
+                                 .format(variable, len(timeseries.times), len(timeseries.values)))
+
+            timeseries_times_sec = self.io.times_sec
+
+            if not np.array_equal(timeseries_times_sec, timeseries.times):
+                if check_consistency:
+                    raise ValueError(
+                        'IOMixin: Trying to set timeseries {} with different times '
+                        '(in seconds) than the imported timeseries. Please make sure the '
+                        'timeseries covers all timesteps of the longest '
+                        'imported timeseries.'.format(variable)
+                    )
+
+                # Determine position of first times of added timeseries within the
+                # import times. For this we assume that both time ranges are ordered,
+                # and that the times of the added series is a subset of the import
+                # times.
+                t_pos = bisect.bisect_left(timeseries_times_sec, timeseries.times[0])
+
+                # Construct a new values range with length of self.io.get_times()
+                values = stretch_values(timeseries.values, t_pos)
+            else:
+                values = timeseries.values
+
+        else:
+            if check_consistency and len(self.times()) != len(timeseries):
+                raise ValueError('IOMixin: Trying to set values for {} with a different '
+                                 'length ({}) than the forecast length. Please make sure the '
+                                 'values covers all timesteps of the longest imported timeseries (length {}).'
+                                 .format(variable, len(timeseries), len(self.times())))
+
+            # If times is not supplied with the timeseries, we add the
+            # forecast times range to a new Timeseries object. Hereby
+            # we assume that the supplied values stretch from T0 to end.
+            t_pos = len(self.io.datetimes) - len(timeseries)
+
+            # Construct a new values range with length of self.io.get_times()
+            values = stretch_values(timeseries, t_pos)
+
+        self.io.set_timeseries(self.io.datetimes, values, variable, ensemble_member)
+
+    def min_timeseries_id(self, variable: str) -> str:
+        """
+        Returns the name of the lower bound timeseries for the specified variable.
+
+        :param variable: Variable name.
+        """
+        return '_'.join((variable, 'Min'))
+
+    def max_timeseries_id(self, variable: str) -> str:
+        """
+        Returns the name of the upper bound timeseries for the specified variable.
+
+        :param variable: Variable name.
+        """
+        return '_'.join((variable, 'Max'))
+
+    @cached
+    def bounds(self):
+        # Call parent class first for default values.
+        bounds = super().bounds()
+
+        io_times = self.io.times_sec
+        t_pos = bisect.bisect_left(io_times, self.initial_time)
+
+        # Load bounds from timeseries
+        for variable in self.dae_variables['free_variables']:
+            variable_name = variable.name()
+
+            m, M = None, None
+
+            timeseries_id = self.min_timeseries_id(variable_name)
+            try:
+                _, values = self.io.get_timeseries_sec(timeseries_id, 0)
+                m = values[t_pos:]
+            except KeyError:
+                pass
+            else:
+                if logger.getEffectiveLevel() == logging.DEBUG:
+                    logger.debug("Read lower bound for variable {}".format(variable_name))
+
+            timeseries_id = self.max_timeseries_id(variable_name)
+            try:
+                _, values = self.io.get_timeseries_sec(timeseries_id, 0)
+                M = values[t_pos:]
+            except KeyError:
+                pass
+            else:
+                if logger.getEffectiveLevel() == logging.DEBUG:
+                    logger.debug("Read upper bound for variable {}".format(variable_name))
+
+            # Replace NaN with +/- inf, and create Timeseries objects
+            if m is not None:
+                m[np.isnan(m)] = np.finfo(m.dtype).min
+                m = Timeseries(io_times[t_pos:], m)
+            if M is not None:
+                M[np.isnan(M)] = np.finfo(M.dtype).max
+                M = Timeseries(io_times[t_pos:], M)
+
+            # Store
+            if m is not None or M is not None:
+                bounds[variable_name] = (m, M)
+        return bounds
+
+    @cached
+    def history(self, ensemble_member):
+        # Load history
+        history = AliasDict(self.alias_relation)
+
+        end_index = bisect.bisect_left(self.io.times_sec, self.initial_time) + 1
+
+        variable_list = self.dae_variables['states'] + self.dae_variables['algebraics'] + \
+            self.dae_variables['control_inputs'] + self.dae_variables['constant_inputs']
+
+        for variable in variable_list:
+            variable = variable.name()
+            try:
+                times, values = self.io.get_timeseries_sec(variable, ensemble_member)
+                history[variable] = Timeseries(
+                    times[:end_index],
+                    values[:end_index]
+                )
+            except KeyError:
+                pass
+            else:
+                if logger.getEffectiveLevel() == logging.DEBUG:
+                    logger.debug("IOMixin: Read history for state {}".format(variable))
+        return history
+
+    @cached
+    def seed(self, ensemble_member):
+        # Call parent class first for default values.
+        seed = super().seed(ensemble_member)
+
+        # Load seeds
+        for variable in self.dae_variables['free_variables']:
+            variable = variable.name()
+            try:
+                s = Timeseries(
+                    *self.io.get_timeseries_sec(variable, ensemble_member)
+                )
+            except KeyError:
+                pass
+            else:
+                if logger.getEffectiveLevel() == logging.DEBUG:
+                    logger.debug("IOMixin: Seeded free variable {}".format(variable))
+                # A seeding of NaN means no seeding
+                s.values[np.isnan(s.values)] = 0.0
+                seed[variable] = s
+        return seed
+
+    @cached
+    def constant_inputs(self, ensemble_member):
+        # Call parent class first for default values.
+        constant_inputs = super().constant_inputs(ensemble_member)
+
+        # Load inputs from timeseries
+        for variable in self.dae_variables['constant_inputs']:
+            variable = variable.name()
+            try:
+                timeseries = Timeseries(
+                    *self.io.get_timeseries_sec(variable, ensemble_member)
+                )
+            except KeyError:
+                pass
+            else:
+                inds = timeseries.times >= self.initial_time
+                if np.any(np.isnan(timeseries.values[inds])):
+                    raise Exception("IOMixin: Constant input {} contains NaN".format(variable))
+                constant_inputs[variable] = timeseries
+                if logger.getEffectiveLevel() == logging.DEBUG:
+                    logger.debug("IOMixin: Read constant input {}".format(variable))
+        return constant_inputs
+
+    def timeseries_at(self, variable, t, ensemble_member=0):
+        return self.interpolate(t, *self.io.get_timeseries_sec(variable, ensemble_member))
+
+    @property
+    def output_variables(self):
+        variables = super().output_variables
+        variables.extend([ca.MX.sym(variable) for variable in self.__output_timeseries])
+        return variables
+
+    def get_forecast_index(self):
+        """
+        Deprecated, use `io.reference_datetime` and `io.datetimes`, or override behavior using
+        :py:meth:`OptimizationProblem.times` and/or :py:attr:`OptimizationProblem.initial_time`.
+        """
+        warnings.warn('get_forecast_index() is deprecated and will be removed in the future', FutureWarning)
+        return bisect.bisect_left(self.io.datetimes, self.io.reference_datetime)

tests/optimization/test_io_mixin.py
@@ -1,0 +1,183 @@
+import bisect
+import logging
+from datetime import datetime, timedelta
+from unittest import TestCase
+
+import casadi as ca
+
+import numpy as np
+
+from rtctools.optimization.collocated_integrated_optimization_problem import (
+    CollocatedIntegratedOptimizationProblem
+)
+from rtctools.optimization.io_mixin import IOMixin
+from rtctools.optimization.modelica_mixin import ModelicaMixin
+from rtctools.optimization.timeseries import Timeseries
+
+from .data_path import data_path
+
+logger = logging.getLogger("rtctools")
+logger.setLevel(logging.WARNING)
+
+
+class DummyIOMixin(IOMixin):
+    def read(self):
+        # fill with dummy data
+        ref_datetime = datetime(2000, 1, 1)
+        self.io.reference_datetime = ref_datetime
+        times_sec = [-7200, -3600, 0, 3600, 7200, 9800]
+        datetimes = [ref_datetime + timedelta(seconds=x) for x in times_sec]
+
+        values = {
+            'constant_input': [1.1, 1.4, 0.9, 1.2, 1.5, 1.7],
+            'u_Min': [0.5, 0.2, 0.3, 0.1, 0.4, 0.0],
+            'u_Max': [2.1, 2.2, 2.0, 2.4, 2.5, 2.3],
+            'alias': [3.1, 3.2, 3.3, 3.4, 3.5, 3.6]  # alias of 'x'
+        }
+
+        for key, value in values.items():
+            self.io.set_timeseries(datetimes, np.array(value), key)
+
+    def write(self):
+        pass
+
+
+class Model(DummyIOMixin, ModelicaMixin, CollocatedIntegratedOptimizationProblem):
+
+    def __init__(self, **kwargs):
+        kwargs["model_name"] = kwargs.get("model_name", "Model")
+        kwargs["input_folder"] = data_path()
+        kwargs["output_folder"] = data_path()
+        kwargs["model_folder"] = data_path()
+        super().__init__(**kwargs)
+
+    def objective(self, ensemble_member):
+        # Quadratic penalty on state 'x' at final time
+        xf = self.state_at("x", self.times()[-1])
+        f = xf ** 2
+        return f
+
+    def constraints(self, ensemble_member):
+        # No additional constraints
+        return []
+
+    def compiler_options(self):
+        compiler_options = super().compiler_options()
+        compiler_options["cache"] = False
+        compiler_options['library_folders'] = []
+        return compiler_options
+
+
+class TestOptimizationProblem(TestCase):
+    """
+    Tests the default methods from OptimizationProblem
+    """
+
+    def setUp(self):
+        self.problem = Model()
+        self.problem.read()
+        self.tolerance = 1e-6
+
+    def test_get_timeseries(self):
+        timeseries = self.problem.get_timeseries('constant_input')
+        expected_times = [-7200, -3600, 0, 3600, 7200, 9800]
+        self.assertTrue(np.array_equal(timeseries.times, expected_times))
+        expected_values = [1.1, 1.4, 0.9, 1.2, 1.5, 1.7]
+        self.assertTrue(np.array_equal(timeseries.values, expected_values))
+
+        timeseries_x = self.problem.get_timeseries('x')
+        self.assertTrue(np.array_equal(timeseries_x.times, expected_times))
+        expected_values = [3.1, 3.2, 3.3, 3.4, 3.5, 3.6]
+        self.assertTrue(np.array_equal(timeseries_x.values, expected_values))
+
+    def test_set_timeseries_with_timeseries(self):
+        times = self.problem.io.times_sec
+        values = [0.1, 1.1, 2.1, 3.1, 4.1, 5.1]
+        self.problem.set_timeseries('newVar', Timeseries(times, values))
+
+        actual_series = self.problem.get_timeseries('newVar')
+        self.assertTrue(np.array_equal(actual_series.values, values))
+        self.assertTrue(np.array_equal(actual_series.times, times))
+
+        # test if it was actually stored in the internal data store
+        _, actual_values = self.problem.io.get_timeseries_sec('newVar')
+        self.assertTrue(np.array_equal(actual_values, values))
+
+        # now let's do this again but only give part of the values
+        values = [1.1, 2.1, 3.1]
+
+        # with check_consistency=True (default) we should get a ValueError
+        with self.assertRaises(ValueError):
+            self.problem.set_timeseries('partialSeries', Timeseries(times[-3:], values))
+
+        self.problem.set_timeseries('partialSeries', Timeseries(times[-3:], values), check_consistency=False)
+
+        actual_series = self.problem.get_timeseries('partialSeries')
+        self.assertTrue(np.array_equal(actual_series.times, times))
+        self.assertTrue(np.array_equal(actual_series.values[-3:], values))
+        self.assertTrue(np.all(np.isnan(actual_series.values[:-3])))
+
+    def test_set_timeseries_with_array(self):
+        times = self.problem.times()
+        values = np.ones(times.shape)
+        self.problem.set_timeseries('newVar', values)
+
+        actual_series = self.problem.get_timeseries('newVar')
+        forecast_index = bisect.bisect_left(self.problem.io.datetimes, self.problem.io.reference_datetime)
+        self.assertTrue(np.array_equal(actual_series.values[forecast_index:], values))
+        self.assertTrue(np.all(np.isnan(actual_series.values[:forecast_index])))
+
+    def test_timeseries_at(self):
+        times = self.problem.io.times_sec
+        values = times.astype(dtype=np.float64) / 10
+        self.problem.set_timeseries('myVar', Timeseries(times, values))
+        actual = self.problem.timeseries_at('myVar', times[0])
+        self.assertEqual(actual, times[0] / 10)
+
+        actual = self.problem.timeseries_at('myVar', (times[0] + times[1]) / 2)
+        self.assertEqual(actual, (values[0] + values[1]) / 2)
+
+    def test_bounds(self):
+        bounds = self.problem.bounds()
+        self.assertEqual(bounds['x'], [float("-inf"), float("inf")])
+
+        min_u = bounds['u'][0]
+        max_u = bounds['u'][1]
+
+        expected_times = [0, 3600, 7200, 9800]
+        self.assertTrue(np.array_equal(min_u.times, expected_times))
+        self.assertTrue(np.array_equal(max_u.times, expected_times))
+
+        expected_min_values = [0.3, 0.1, 0.4, 0.0]
+        self.assertTrue(np.array_equal(min_u.values, expected_min_values))
+
+        expected_max_values = [2.0, 2.4, 2.5, 2.3]
+        self.assertTrue(np.array_equal(max_u.values, expected_max_values))
+
+    def test_history(self):
+        history = self.problem.history(0)
+
+        expected_times = [-7200, -3600, 0]
+        self.assertTrue(np.array_equal(history['x'].times, expected_times))
+        self.assertTrue(np.array_equal(history['constant_input'].times, expected_times))
+
+        expected_history_x = [3.1, 3.2, 3.3]
+        self.assertTrue(np.array_equal(history['x'].values, expected_history_x))
+        expected_history_u = [1.1, 1.4, 0.9]
+        self.assertTrue(np.array_equal(history['constant_input'].values, expected_history_u))
+
+    def test_seed(self):
+        # add another variable containing some nans
+        self.problem.io.set_timeseries_sec(self.problem.io.times_sec,
+                                           np.array([np.nan, 0.1, 0.2, np.nan, 3.1, np.nan]),
+                                           'some_missing')
+        self.problem.dae_variables['free_variables'].append(ca.MX().sym('some_missing'))
+
+        seed = self.problem.seed(0)
+        self.assertTrue(np.array_equal(seed['x'].values, [3.1, 3.2, 3.3, 3.4, 3.5, 3.6]))
+        self.assertTrue(np.array_equal(seed['alias'].values, [3.1, 3.2, 3.3, 3.4, 3.5, 3.6]))
+        self.assertTrue(np.array_equal(seed['some_missing'].values, [0, 0.1, 0.2, 0, 3.1, 0]))
+
+    def test_constant_inputs(self):
+        constant_inputs = self.problem.constant_inputs(0)
+        self.assertTrue(np.array_equal(constant_inputs['constant_input'].values, [1.1, 1.4, 0.9, 1.2, 1.5, 1.7]))

commit aee225c06468f32db97864740a634c5d0bde609d

IOMixin: Fix setting timeseries with subset of import times
We raised an Exception when calling set_timeseries where the times were
a subset of the import times. This is a regression in 623fc2cf (IO
refactor). We should allow such cases, and just stretch the values. Note
that this goes both for calling set_timeseries with a Timeseries, and
for calling it with a np.array.

src/rtctools/optimization/io_mixin.py
@@ -108,12 +108,13 @@ class IOMixin(OptimizationProblem, metaclass=ABCMeta):
 
             if not np.array_equal(timeseries_times_sec, timeseries.times):
                 if check_consistency:
-                    raise ValueError(
-                        'IOMixin: Trying to set timeseries {} with different times '
-                        '(in seconds) than the imported timeseries. Please make sure the '
-                        'timeseries covers all timesteps of the longest '
-                        'imported timeseries.'.format(variable)
-                    )
+                    if not set(timeseries_times_sec).issuperset(timeseries.times):
+                        raise ValueError(
+                            'IOMixin: Trying to set timeseries {} with different times '
+                            '(in seconds) than the imported timeseries. Please make sure the '
+                            'timeseries covers all timesteps of the longest '
+                            'imported timeseries.'.format(variable)
+                        )
 
                 # Determine position of first times of added timeseries within the
                 # import times. For this we assume that both time ranges are ordered,
@@ -127,16 +128,25 @@ class IOMixin(OptimizationProblem, metaclass=ABCMeta):
                 values = timeseries.values
 
         else:
-            if check_consistency and len(self.times()) != len(timeseries):
-                raise ValueError('IOMixin: Trying to set values for {} with a different '
-                                 'length ({}) than the forecast length. Please make sure the '
-                                 'values covers all timesteps of the longest imported timeseries (length {}).'
+            timeseries_times_sec = self.io.times_sec
+
+            if check_consistency:
+                if len(self.times()) != len(timeseries):
+                    raise ValueError('IOMixin: Trying to set values for {} with a different '
+                                 'length ({}) than the forecast length ({}).'
                                  .format(variable, len(timeseries), len(self.times())))
+                elif not set(timeseries_times_sec).issuperset(self.times()):
+                    raise ValueError(
+                        'IOMixin: Trying to set timeseries {} with different times '
+                        '(in seconds) than the imported timeseries. Please make sure the '
+                        'timeseries covers all timesteps of the longest '
+                        'imported timeseries.'.format(variable)
+                    )
 
             # If times is not supplied with the timeseries, we add the
             # forecast times range to a new Timeseries object. Hereby
             # we assume that the supplied values stretch from T0 to end.
-            t_pos = len(self.io.datetimes) - len(timeseries)
+            t_pos = bisect.bisect_left(timeseries_times_sec, self.initial_time)
 
             # Construct a new values range with length of self.io.get_times()
             values = stretch_values(timeseries, t_pos)

tests/optimization/test_io_mixin.py
@@ -41,6 +41,12 @@ class DummyIOMixin(IOMixin):
     def write(self):
         pass
 
+    def times(self, variable=None):
+        if hasattr(self, '_overrule_times'):
+            return self._overrule_times
+        else:
+            return super().times(variable)
+
 
 class Model(DummyIOMixin, ModelicaMixin, CollocatedIntegratedOptimizationProblem):
 
@@ -106,26 +112,63 @@ class TestOptimizationProblem(TestCase):
         # now let's do this again but only give part of the values
         values = [1.1, 2.1, 3.1]
 
-        # with check_consistency=True (default) we should get a ValueError
-        with self.assertRaises(ValueError):
-            self.problem.set_timeseries('partialSeries', Timeseries(times[-3:], values))
+        # with check_consistency=True (default) we should be fine as long
+        # as the timeseries times are a subset of the import times
+        self.problem.set_timeseries('partialSeries1', Timeseries(times[-3:], values))
+        actual_series = self.problem.get_timeseries('partialSeries1')
+        self.assertTrue(np.array_equal(actual_series.times, times))
+        self.assertTrue(np.array_equal(actual_series.values[-3:], values))
+        self.assertTrue(np.all(np.isnan(actual_series.values[:-3])))
 
-        self.problem.set_timeseries('partialSeries', Timeseries(times[-3:], values), check_consistency=False)
+        wrong_times = times[-3:].copy()
+        wrong_times[-1] -= 1
+        with self.assertRaisesRegex(ValueError, "different times"):
+            self.problem.set_timeseries('partialSeries2', Timeseries(wrong_times, values))
 
-        actual_series = self.problem.get_timeseries('partialSeries')
+        # Without the check, we will also get through with wrong times
+        self.problem.set_timeseries('partialSeries2', Timeseries(wrong_times, values), check_consistency=False)
+
+        actual_series = self.problem.get_timeseries('partialSeries2')
         self.assertTrue(np.array_equal(actual_series.times, times))
         self.assertTrue(np.array_equal(actual_series.values[-3:], values))
         self.assertTrue(np.all(np.isnan(actual_series.values[:-3])))
 
     def test_set_timeseries_with_array(self):
         times = self.problem.times()
-        values = np.ones(times.shape)
+        values = np.random.random(times.shape)
         self.problem.set_timeseries('newVar', values)
 
         actual_series = self.problem.get_timeseries('newVar')
         forecast_index = bisect.bisect_left(self.problem.io.datetimes, self.problem.io.reference_datetime)
         self.assertTrue(np.array_equal(actual_series.values[forecast_index:], values))
         self.assertTrue(np.all(np.isnan(actual_series.values[:forecast_index])))
+
+        # with check_consistency=True (default) we should be fine as long
+        # as self.times() returns subset of the import times
+        orig_times = self.problem.times()
+        self.problem._overrule_times = orig_times[1:]
+        self.problem.set_timeseries('partialSeries1', values[1:])
+        actual_series = self.problem.get_timeseries('partialSeries1')
+        self.assertTrue(np.array_equal(actual_series.times, self.problem.io.times_sec))
+        self.assertTrue(np.array_equal(actual_series.values[-3:], values[1:]))
+        self.assertTrue(np.all(np.isnan(actual_series.values[:-3])))
+
+        with self.assertRaisesRegex(ValueError, "different length"):
+            self.problem.set_timeseries('partialSeries1', values)
+
+        wrong_times = orig_times.copy()[1:]
+        wrong_times[-1] -= 1
+        self.problem._overrule_times = wrong_times
+        with self.assertRaisesRegex(ValueError, "different times"):
+            self.problem.set_timeseries('partialSeries2', values[1:])
+
+        # Without the check, we will also get through with wrong times
+        self.problem.set_timeseries('partialSeries2', values[1:], check_consistency=False)
+
+        actual_series = self.problem.get_timeseries('partialSeries2')
+        self.assertTrue(np.array_equal(actual_series.times, self.problem.io.times_sec))
+        self.assertTrue(np.array_equal(actual_series.values[-3:], values[1:]))
+        self.assertTrue(np.all(np.isnan(actual_series.values[:-3])))
 
     def test_timeseries_at(self):
         times = self.problem.io.times_sec
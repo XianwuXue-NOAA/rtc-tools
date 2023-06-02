ce577c084f26269ce1ed904a041ed71e40ae892d
Sjoerd Geevers <Sjoerd.Geevers@deltares.nl>
2023-06-02 13:53:40 +0200

    examples: demonstrate qpsol with HiGHS
    Demonstrate solving a quadratic problem using casadi.qpsol with
    HiGHS plugin.

--- examples/basic/src/example.py
+++ examples/basic/src/example.py
@@ -4,17 +4,25 @@ from rtctools.optimization.csv_mixin import CSVMixin
 from rtctools.optimization.modelica_mixin import ModelicaMixin
 from rtctools.util import run_optimization_problem
 
+import casadi as ca
+
 
 class Example(CSVMixin, ModelicaMixin, CollocatedIntegratedOptimizationProblem):
     """
     A basic example for introducing users to RTC-Tools 2
     """
 
+    def solver_options(self):
+        options = super().solver_options()
+        options['casadi_solver'] = ca.qpsol
+        options['solver'] = 'highs'
+        return options
+
     def objective(self, ensemble_member):
         # Minimize water pumped. The total water pumped is the integral of the
         # water pumped from the starting time until the stoping time. In
         # practice, self.integral() is a summation of all the discrete states.
-        return self.integral('Q_release', ensemble_member=ensemble_member)
+        return self.integral('Q_release', ensemble_member=ensemble_member) ** 2
 
     def path_constraints(self, ensemble_member):
         # Call super() class to not overwrite default behaviour
